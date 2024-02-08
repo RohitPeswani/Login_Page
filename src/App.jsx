129d3e5850ef3176ab0ba8d6d90d3291af680b71
Rohit Peswani <rohitpeswani28@gmail.com>
2024-02-08 10:37:02 +0530

    fc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+
 import Navbar from './components/Navbar'
 import Login from './components/Login'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
